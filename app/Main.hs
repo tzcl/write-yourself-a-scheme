2480afd6f65093274b16193aadd8f695d4111542
Toby Law <toby@tzcl.me>
2021-08-20 17:48:30 +1000

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,61 @@
+module Main where
+
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+_/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+escapedChars :: Parser Char
+escapedChars = do
+  char '\\'
+  x <- oneOf "\\\"nrt"
+  return $ case x of
+    'n' -> '\n'
+    'r' -> '\r'
+    't' -> '\t'
+    _ -> x
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many $ escapedChars <|> noneOf "\"\\"
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right _ -> "Found value"
+
+main :: IO ()
+main = do
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
